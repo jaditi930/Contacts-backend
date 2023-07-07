6f44efe9aa0345ed8826b0c9654f823a588761fc
Aditi Jain <jaditi930@gmail.com>
2023-07-07 16:35:43 +0530

    setting routes

--- server.js
+++ server.js
@@ -4,6 +4,22 @@ const dotenv=require("dotenv").config()
 const app=express()
 const port=process.env.PORT
 
+app.get("/api/products/",(req,res)=>{
+    res.status(200).json({message:"Get all products"})
+})
+
+app.post("/api/products/",(req,res)=>{
+    res.status(200).json({message:"Product created successfully"})
+})
+app.put("/api/products/:id/",(req,res)=>{
+    id=req.params.id
+    res.status(200).json({message:`Product ${id} updated successfully`})
+})
+app.delete("/api/products/:id/",(req,res)=>{
+    id=req.params.id
+    res.status(200).json({message:`Product ${id} deleted successfully`})
+})
+
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`)
 })
