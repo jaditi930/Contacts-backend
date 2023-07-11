292df46242ea4ef26910ca765f918037d8242749
Aditi Jain <jaditi930@gmail.com>
2023-07-11 17:10:01 +0530

    set cookie

--- server.js
+++ server.js
@@ -4,13 +4,7 @@ const errorHandler=require("./middleware/errorHandler")
 const app=express()
 const connectDb=require("./dbConnection")
 const bodyParser = require('body-parser');
-const cors=require('cors');
 app.use(bodyParser.urlencoded({limit: '5000mb', extended: true, parameterLimit: 100000000000}));
-app.use(function(req, res, next) {  
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});  
 
 const port=process.env.PORT
 
