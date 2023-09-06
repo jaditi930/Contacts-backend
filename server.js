d0cbc12b253b8c6ef0912c98b01e50fa972e1108
Aditi Jain <jaditi930@gmail.com>
2023-09-07 01:33:12 +0530

    origin updated

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const port=process.env.PORT
 connectDb()
 
 const corsOptions ={
-    origin:['https://contactly.onrender.com','http://localhost:3000'], 
+    origin:['https://contact-manager-app-xy4o.onrender.com/','http://localhost:3000'], 
     credentials:true,            
 
 }
